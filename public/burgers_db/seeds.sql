ae5189742ec1a8b1b319372c4eeec27cb62f6b5e
Mark Piscioneri <markjpiscioneri77@gmail.com>
2018-11-03 18:55:13 -0400

    redo of db

--- public/burgers_db/seeds.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO burgers (burger_name) VALUES ('Cheese Burger in Paradise');
-INSERT INTO burgers (burger_name) VALUES ('A1 Burger');
-INSERT INTO burgers (burger_name) VALUES ('Kobe Beef Burger');
-INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Burger');
