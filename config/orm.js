a4b56aff777db26d6145837f6c97e965ba2deefc
Mark Piscioneri <markjpiscioneri77@gmail.com>
2018-11-05 17:31:42 -0500

    functions in orm file to add and devour burgers

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,38 @@
-// Import MySQL connection
-var connection = require("./connection,js");
+// Import MySQL connection...local folder
+var connection = require("./connection.js");
+
+// set up functions to SELECT and INSERT burgers
+var orm = {
+    selectAll: function (cb) {
+        var queryString = "SELECT * FROM burgers";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+
+    insertOne: function (burger,cb) {
+        var queryString = "INSERT INTO burgers (burger_name) VALUES (?)";
+        connection.query(queryString, [burger], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+// function to UPDATE showing devoured
+    updateOne: function (id, cb) {
+        var queryString = "UPDATE burgers SET devoured = true WHERE id = ?";
+        connection.query(queryString, [id], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result)
+        });
+    }
+};
+
+module.exports = orm;
+
