2506e4f4574b96cd6d896f770b5c388252a5836c
Mark Piscioneri <markjpiscioneri77@gmail.com>
2018-11-05 18:00:23 -0500

    set up path to burgers.js file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,9 @@
 // create routes here
 //  express, router details in this file
+var express = require('express');
+
+// configure router
+var router = express.Router();
+
+// Set var for burgers and set path
+var burger = require("../models/burgers.js");
