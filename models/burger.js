aaaf0bb6eb830f39a745190d4d91306cd06f6088
Mark Piscioneri <markjpiscioneri77@gmail.com>
2018-11-05 18:23:16 -0500

    finished pseudocode for burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,7 @@
 // need to import the ORM to create the functions to interact with the db
+// set path to the orm.js
+// functions similar in set-up to the orm.js file
+var orm = require('../config/orm.js');
 
+// set up variable for burger with - select, insert and update functions
+// need a module exports for burger 
