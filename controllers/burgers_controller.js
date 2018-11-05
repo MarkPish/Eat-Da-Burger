603c0f399f47404c23d73a6bac11935875441b9a
Mark Piscioneri <markjpiscioneri77@gmail.com>
2018-11-05 18:05:19 -0500

    adding router functions-debug

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,3 +7,18 @@ var router = express.Router();
 
 // Set var for burgers and set path
 var burger = require("../models/burgers.js");
+
+//.get...check details from activities...cats
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+});
+
+// post?
+
+// put?...check past activities and class notes from 11-3
