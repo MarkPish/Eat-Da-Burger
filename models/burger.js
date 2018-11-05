3570bb09bcf1609134c1f36b1b597024050025f8
Mark Piscioneri <markjpiscioneri77@gmail.com>
2018-11-05 18:32:07 -0500

    follows with activities orm file

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,30 @@
 // need to import the ORM to create the functions to interact with the db
 // set path to the orm.js
-// functions similar in set-up to the orm.js file
 var orm = require('../config/orm.js');
 
-// set up variable for burger with - select, insert and update functions
+//  call back functions similar in set-up to the orm.js file
+// set up variable for burger with - select, insert and update functions all inside the variable
+
+var burger = {
+    selectAll: function (cb) {
+        orm.selectAll(function (res) {
+            cb(res);
+        });
+    },
+
+    insertOne: function (burger, cb) {
+        orm.insertOne(burger, function (res) {
+            cb(res);
+        });
+    },
+
+    updateOne: function (id, cb) {
+        orm.updateOne([id], function (res) {
+            cb(res);
+        });
+    }
+
+};
+
 // need a module exports for burger 
+module.exports = burger;
